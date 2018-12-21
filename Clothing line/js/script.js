570f602e68547b2a7f52cd2e7b755b221b94cb81
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2018-12-21 21:19:10 +0530

    Periscope changes modified

--- Clothing line/js/script.js
+++ Clothing line/js/script.js
@@ -21,14 +21,23 @@ $(window).scroll(function() {
   });
 
   if(wScroll > $('#gallery').offset().top -500) {
-  //  console.log('asd');
+    let aspectRatio = wScroll / window.innerHeight;
+    console.log('aspect ration '+aspectRatio);
     $('.models').each(function(i){
-        console.log("i : "+i);
+      //  console.log("i : "+i);
         setTimeout(function(){
           $('.models').eq(i).addClass('modelVisible');
         } , (1+i)*100);
     });
 
+
+    let periscopeOffset = 2.12 ;
+
+    if(wScroll > periscopeOffset ){
+      $('#periscopeWindow').css({'background-position' : 'center '+(aspectRatio - periscopeOffset)*150 + 'px' });
+    }else{
+        $('#periscopeWindow ').css({'background-position' : 'center 0px' });
+    }
 
   }else{
     $('.models').removeClass('modelVisible');
