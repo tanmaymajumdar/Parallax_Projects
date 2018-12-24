e6316df1ad94307dcf81f5ac3381f34295f9a4c0
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2018-12-24 12:32:03 +0530

    Thumbnail added

--- Clothing line/js/script.js
+++ Clothing line/js/script.js
@@ -1,9 +1,7 @@
 $(window).scroll(function() {
 
+
   var wScroll = $(this).scrollTop();
-
-
-  //  console.log('translate('+ wScrol +','+ wScroll/2 +'%)');
 
   $('.midground').css({
     'transform':'translate('+ 0+'%' +','+ wScroll/2 +'%)'
@@ -22,7 +20,6 @@ $(window).scroll(function() {
 
   if(wScroll > $('#gallery').offset().top -500) {
     var aspectRatio = wScroll / window.innerHeight;
-    console.log('aspect ration '+aspectRatio);
     $('.models').each(function(i){
       //  console.log("i : "+i);
         setTimeout(function(){
@@ -32,6 +29,8 @@ $(window).scroll(function() {
 
 
     let periscopeOffset = 2.12 ;
+
+
 
     if(wScroll > periscopeOffset ){
       $('#periscopeWindow').css({'background-position' : 'center '+(aspectRatio - periscopeOffset)*150 + 'px' });
@@ -47,4 +46,56 @@ $(window).scroll(function() {
 
   $('.promoTextContainer').css({'opacity' : periscopeOpacity+''});
 
+//  console.log(wScroll / window.innerHeight);
+
+  let screenOffset = wScroll / window.innerHeight;
+  let blogOffset = 2 ;
+
+  var render = true;
+
+  if(screenOffset >= blogOffset && render) {
+
+    let offset = $('.blogs').offset().top-$(window).height() - wScroll - 750 ;
+
+      console.log($('.blogs').offset().top+","+$(window).height()+","+wScroll);
+
+      let x = Math.abs(Math.sin(wScroll/300))*280*(-1);
+      let y = Math.sin(wScroll/300)*100*(-1) ;
+
+      let yy = -y
+
+      console.log(yy);
+
+
+
+  //  let myyOffset = Math.min( 0 , x);
+    $('.post1').css({'transform' : 'translate('+x+'px,'+y+'px)'});
+    $('.post2').css({'transform' : 'translate('+0+'px,'+yy+'px)'});
+    $('.post3').css({'transform' : 'translate('+-x+'px,'+y+'px)'});
+  }
+
+});
+
+
+$(document).ready(function(){
+  $(".readMore").on('click' , function(){
+
+    let className = '.'+$(this).attr('id');
+
+    let visibility = $(className).css('display');
+
+    if(visibility == 'none') {
+      visibility = 'inline';
+      $(this).html('Show less');
+    }
+    else {
+      visibility = 'none';
+      $(this).html('Read more');
+    }
+
+    $(className).css({'display' : visibility});
+
+
+
+  } );
 });
